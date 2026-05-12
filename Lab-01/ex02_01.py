9ef426ba33c17d19f6050045e45a1a1060587682
longnuyen08763-cpu <longnguyen08763@gmail.com>
2026-05-12 11:28:00 +0700

    lab01 hello world

---
+++ Lab-01/ex02_01.py
@@ -0,0 +1,4 @@
+#nhập tên và tuổi người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = int(input("Nhập tuổi của bạn: "))
+print("Xin chào {ten}, bạn năm nay {tuoi} tuổi.")
---
+++ Lab-01/ex02_02.py
@@ -0,0 +1,9 @@
+#nhập bán kính từ người dùng
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+#tính diện tích hình tròn 
+dien_tich = 3.14 * (ban_kinh ** 2)
+#tính chu diện tích của hình tròn ra máy tính
+print("Diện tích của hình tròn là :", dien_tich)
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+
+dien_tich = 3.14 * ban_kinh ** 2)
