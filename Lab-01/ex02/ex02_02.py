a1e11b4b3757bba7be768e7dd17ef0296ba719dc
longnguyen08763-cpu <longnguyen08763@gmail.com>
2026-05-14 17:05:35 +0700

    lab01

--- Lab-01/ex02/ex02_02.py
+++ Lab-01/ex02/ex02_02.py
@@ -6,4 +6,4 @@ dien_tich = 3.14 * (ban_kinh ** 2)
 print("Diện tích của hình tròn là :", dien_tich)
 ban_kinh = float(input("Nhập bán kính hình tròn: "))
 
-dien_tich = 3.14 * ban_kinh ** 2)
+dien_tich = (3.14 * ban_kinh ** 2)
